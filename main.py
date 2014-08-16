0186030b7b7eddf8853f11dc15c3c767305d322a
Kelong Cong <kelongcong@gmail.com>
2014-08-16 14:19:59 +0100

    adoped csv format

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+#!/bin/python3
 
 # a wget example
 # wget -d --header "Host: www.nyaa.se" --header "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0" --header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" --header "Accept-Language: en-gb,en;q=0.5" --header "Referer: http://www.nyaa.se/?page=view&tid=587070" --header "Connection: keep-alive" "http://www.nyaa.se/?page=download&tid=587070" -O myfile.torrent
 
+"""
+example usage:
+    watch -n 120 ./main.py
+"""
+
 import urllib.request
 import urllib.parse
 import urllib.error
+import csv
 import time
 import re # regex
+import os
+
 
 NYAA="http://www.nyaa.se/"
 SEARCH_VAL = { "page":"search", "cats":"0_0", "filter":"0", }
@@ -30,22 +39,26 @@ def save_torrent_file(filename, data):
 
 def parse_watchlist(filename):
 
-    # TODO need to check second element is a number
-    # TODO use csv library
-    f = open(filename, "r")
-    wlist = [tuple(i.rstrip().split(",")) for i in f.readlines()]
-    f.close()
+    wlist = []
+    count = 0
+
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            wlist.append((row[0], int(row[1]), len(row[1]), count))
+            count += 1
     return wlist
 
 
 def search_torrent(wlist):
 
     download_val = []
-    for x,y in wlist:
-        x = x.replace(" ", "+")
-        x = x.replace("%EPS", y)
+
+    for term, eps, c, i in wlist:
+        term = term.replace(" ", "+")
+        term = term.replace("%EPS", str(eps).zfill(c))
         search_vals = SEARCH_VAL
-        search_vals['term'] = x
+        search_vals['term'] = term
         search_url = NYAA + "?" + urllib.parse.urlencode(search_vals)
         req = urllib.request.Request(search_url)
 
@@ -55,11 +68,11 @@ def search_torrent(wlist):
                 results = response.read().decode("utf-8")
                 m = re.search("page=download&#38;tid=\d+", results).group(0)
                 m = m.replace("#38;", "")
-                download_val.append(m)
+                download_val.append((m,i)) # include the index of csv
             elif response.url.find("page=search") is not -1:
-                print(x + " is not found or has no unique results")
+                print(term + " is not found or has no unique results")
             else:
-                print(x + " unknown error")
+                print(term + " unknown error")
                 print("\t" + response.url)
 
         except Exception as e:
@@ -71,8 +84,8 @@ def search_torrent(wlist):
 
 def download_torrent(download_val):
 
-    count = 0 # number of downloaded file
-    for x in download_val:
+    downloaded_list = []
+    for x, i in download_val:
         download_url = NYAA + "?" + x
         req = urllib.request.Request(download_url, headers = DOWNLOAD_HEADER)
 
@@ -80,22 +93,44 @@ def download_torrent(download_val):
             response = urllib.request.urlopen(req)
             filename = re.search('".*"', response.getheader("Content-Disposition")).group(0)[1:-1]
             save_torrent_file(filename, response.read())
-            count += 1
+            downloaded_list.append(i)
             print("saved " + filename)
 
         except Exception as e:
             print(e.reason)
             print(e.message)
 
-    return count
+    return downloaded_list
+
+
+def update_watchlist(wlist, ulist, filename): # ulist is list needs updating
+
+    with open(filename+".tmp", 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for term, eps, c, i in wlist:
+            if i in ulist:
+                eps += 1
+            row = [term, str(eps).zfill(c)]
+            writer.writerow(row)
+
+    os.remove(filename)
+    os.rename(filename+".tmp", filename)
 
 
 if __name__ == "__main__":
 
-    wlist = parse_watchlist("watchlist.txt")
+    filename = "watchlist.csv"
+
+    wlist = parse_watchlist(filename)
     download_val = search_torrent(wlist)
     ret = download_torrent(download_val)
 
-    print("downloaded " + str(ret) + " file(s).")
+    if len(ret) is not 0:
+        update_watchlist(wlist, ret, filename)
+
+    print("downloaded " + str(len(ret)) + " file(s).")
+
+
+
 
 
