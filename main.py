db4850373dc8b17673974d1a8f20e178bde8cf4b
Kelong Cong <kelongcong@gmail.com>
2014-08-16 20:10:30 +0100

    improved error checking

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 #!/bin/python3
 
-# a wget example
-# wget -d --header "Host: www.nyaa.se" --header "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0" --header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" --header "Accept-Language: en-gb,en;q=0.5" --header "Referer: http://www.nyaa.se/?page=view&tid=587070" --header "Connection: keep-alive" "http://www.nyaa.se/?page=download&tid=587070" -O myfile.torrent
-
 """
 example usage:
     watch -n 120 ./main.py
@@ -11,26 +8,34 @@ example usage:
 import urllib.request
 import urllib.parse
 import urllib.error
-import csv
+import csv # or json?
 import time
 import re # regex
 import os
 
 
 NYAA="http://www.nyaa.se/"
-SEARCH_VAL = { "page":"search", "cats":"0_0", "filter":"0", }
+
+SEARCH_VAL = {
+    "page":"search",
+    "cats":"0_0",
+    "filter":"0",
+}
+
 DOWNLOAD_HEADER = {
- "Host": "www.nyaa.se",
- # "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0",
- "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
- "Accept-Language": "en-gb,en;q=0.5",
- # "Accept-Encoding": "gzip, deflate",
- # "Referer": "http://www.nyaa.se/?page=view&tid=587678",
- "Connection": "keep-alive"
+    "Host": "www.nyaa.se",
+    # "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-gb,en;q=0.5",
+    # "Accept-Encoding": "gzip, deflate",
+    # "Referer": "http://www.nyaa.se/?page=view&tid=587678",
+    "Connection": "keep-alive"
 }
 
 
 def save_torrent_file(filename, data):
+
+    assert(len(filename) > 0)
 
     f = open(filename, "wb+")
     f.write(data)
@@ -39,14 +44,20 @@ def save_torrent_file(filename, data):
 
 def parse_watchlist(filename):
 
+    assert(len(filename) > 0)
+
     wlist = []
     count = 0
 
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            wlist.append((row[0], int(row[1]), len(row[1]), count))
-            count += 1
+            try:
+                wlist.append((row[0], int(row[1]), len(row[1]), count))
+                count += 1
+            except ValueError:
+                print("value error on " + " ,".join(row))
+
     return wlist
 
 
@@ -70,14 +81,14 @@ def search_torrent(wlist):
                 m = m.replace("#38;", "")
                 download_val.append((m,i)) # include the index of csv
             elif response.url.find("page=search") is not -1:
-                print(term + " is not found or has no unique results")
+                print(term + " still on search page")
             else:
-                print(term + " unknown error")
+                print(term + " unknown page")
                 print("\t" + response.url)
 
-        except Exception as e:
-            print(e.reason)
-            print(e.message)
+        except urllib.error.HTTPError as e:
+            print("HTTPError: " + str(e.code))
+            print("\t" + e.reason)
 
     return download_val
 
@@ -96,14 +107,16 @@ def download_torrent(download_val):
             downloaded_list.append(i)
             print("saved " + filename)
 
-        except Exception as e:
-            print(e.reason)
-            print(e.message)
+        except urllib.error.HTTPError as e:
+            print("HTTPError: " + str(e.code))
+            print("\t" + e.reason)
 
     return downloaded_list
 
 
 def update_watchlist(wlist, ulist, filename): # ulist is list needs updating
+
+    assert(len(ulist) > 0)
 
     with open(filename+".tmp", 'w') as csvfile:
         writer = csv.writer(csvfile)
@@ -113,11 +126,12 @@ def update_watchlist(wlist, ulist, filename): # ulist is list needs updating
             row = [term, str(eps).zfill(c)]
             writer.writerow(row)
 
-    os.remove(filename)
     os.rename(filename+".tmp", filename)
 
 
 if __name__ == "__main__":
+
+    # perhaps use a proper web scraper in future version
 
     filename = "watchlist.csv"
 
